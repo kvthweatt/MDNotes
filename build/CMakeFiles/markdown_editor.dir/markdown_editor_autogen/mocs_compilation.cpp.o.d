149c68b834116b9d8246218e71844a0923f0ca1e
Karac Thweatt <kvthweatt@gmail.com>
2025-02-16 19:27:27 -0600

    Removed /build/

--- build/CMakeFiles/markdown_editor.dir/markdown_editor_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/markdown_editor.dir/markdown_editor_autogen/mocs_compilation.cpp.o: \
- /home/karac/MDNotes/build/markdown_editor_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
